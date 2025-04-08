602f817365452f5679068f14c3fd5ec7c81cbfd2
nvv23 <vovatva01@gmail.com>
2025-04-09 01:26:42 +0700

    a

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ const io = initializeSocket(server);
 
 // Middleware truyền io vào request
 app.use((req, res, next) => {
-  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
-  res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
   req.io = io;
   next();
 });
